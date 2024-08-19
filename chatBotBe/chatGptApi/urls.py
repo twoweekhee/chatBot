1df6baf48a81e1fa101ce9fb5e0967fe4e569502
twoweekhee <myjjoo4758@icloud.com>
2024-08-19 16:02:16 +0900

    fe

--- chatBotBe/chatGptApi/urls.py
+++ chatBotBe/chatGptApi/urls.py
@@ -1,11 +1,10 @@
 from django.urls import include, path
 
-from chatGptApi.views import ask_gpt, upload_file
+from chatGptApi.views import ask_gpt
 
 urlpatterns = [
     # path('chatGptApi/', include('chatGptApi.urls')),
     
-    path('upload', upload_file, name='upload_file'),
     path('ask', ask_gpt, name='ask_gpt'),
 
 ]
