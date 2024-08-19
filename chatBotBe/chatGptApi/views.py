9eedcd88f3b4dcde4c2a790fbeaf04244cc0f92a
twoweekhee <myjjoo4758@icloud.com>
2024-08-19 15:59:04 +0900

    refactor

--- chatBotBe/chatGptApi/views.py
+++ chatBotBe/chatGptApi/views.py
@@ -1,5 +1,6 @@
-import json
-import os
+
+import io
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -15,65 +16,78 @@ import logging
 logger = logging.getLogger(__name__)
 
 @csrf_exempt
-def upload_file(request):
-    if request.method == 'POST' and 'file' in request.FILES:
-        file = request.FILES['file']
-        file_name = file.name
-
-        # 파일을 저장할 경로
-        file_path = os.path.join('uploads', file_name)
-        
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-
-        # 파일 저장
-        with open(file_path, 'wb+') as destination:
-            for chunk in file.chunks():
-                destination.write(chunk)
-            
-        
-        logger.info(f"File saved successfully at {file_path}")
-        return JsonResponse({'status': 'success', 'message': f'{file_name} uploaded successfully!', 'file_path': file_path})
-    logger.info("File saved fail at")
-    return JsonResponse({'status': 'fail', 'message': 'File upload failed.'}, status=400)
-
-@csrf_exempt
 def ask_gpt(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        file_path = data.get('filePath')
-        if not file_path:
-            return JsonResponse({'error': 'File path not provided.'}, status=400)
-
+        logger.info(request)
+        file = request.FILES['file']
+        
         client = OpenAI(api_key=OPEN_API_KEY)
 
         prompt = """
-        내가 운영하고 있는 어플에 대한 이번달 리뷰를 만족도(5단계)와 의견으로 나눠서 받았어. 키워드를 바탕으로 요약해서 정리해줘
+        내가 운영하고 있는 어플에 대한 이번달 리뷰를 만족도(5단계)와 의견으로 나눠서 받았어. 첫번째로 만족도의 갯수를 새서 다른 문자 없이 json 결과만 아래와 같은 양식으로 똑같이 바꿔서 반환을 해줘 xAxis는 매우 만족, 만족, 보통, 불만족, 매우 불만족이고 yAxis 는 각 만족도별 갯수야
+        option = {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'bar'
+                }
+            ]
+            };
+        두번째로 이번달 리뷰에서 많이 나온 의견을 바탕으로 어플의 긍정적인 부분과 부정적인 부분을 나눠서 전부 정리해서 알려줘. 모든 의견을 취합해줬으면 좋겠어 소수의 의견이라도 듣고 싶어.
+        세번째로 너가 한 답변이 몇프로의 정확도를 가지고 있는 지 체크해서 왜 100프로 정확도가 아니라고 생각하는 지도 분석해줘.
         """
 
+        # OpenAI 클라이언트 설정
+        client = OpenAI(api_key=OPEN_API_KEY)
+
+        # 파일을 OpenAI API가 처리할 수 있는 형식으로 변환
+        file_bytes = file.read()
+        file_name = file.name
+
         try:
-            response = client.ChatCompletion.create(
-                model="gpt-4",
+            assistant = client.beta.assistants.create(
+                name="Analyst Assistant",
+                instructions="너는 분석 전문가야 내가 올려준 파일을 분석해서 한글로 결과를 도출해줘. 분석하다가 만족도와 의견이 상충되는 경우, 예를 들어 매우 만족인 데 부족한 점을 적은 경우와 같이 이런 경우에 있어서 만족도와 의견은 분리된다고 생각하고 답변해줘. 부족한 점이 있지만 매우 만족하고 있을 수 있잖아. ",
+                model="gpt-4o",
+                tools=[{"type": "file_search"}],
+            )
+            
+            my_file = client.files.create(
+                file=(file_name, io.BytesIO(file_bytes)),
+                purpose="user_data"
+            )
+            
+            thread = client.beta.threads.create(
                 messages=[
                     {
-                        "role": "system",
-                        "content": file_path
-                    },
-                    {
-                        "role": "user",
-                        "content": prompt
-                    },
-                    {
-                        "role": "user",
-                        "content": "피드백이나 부정적인 의견 있으면 그거 취합해서 알려줄래?"
+                    "role": "user",
+                    "content": prompt,
+                    "attachments": [{ "file_id": my_file.id, "tools": [{"type": "file_search"}] }],
                     }
                 ],
-                max_tokens=2000,
-            )
 
-            gpt_response = response.choices[0].message["content"]
-            return JsonResponse({'gpt_response': gpt_response})
+            )
+            run = client.beta.threads.runs.create_and_poll(thread_id=thread.id, assistant_id=assistant.id)
+            messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+
+
+            gpt_response = messages[0].content[0].text.value
+            return JsonResponse({'gpt_response': gpt_response}, json_dumps_params={'ensure_ascii':False})
 
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
 
     return JsonResponse({'error': 'Invalid request method.'}, status=405)
+        
+        
+
+def create_new_thread():
+    thread=OpenAI.beta.threads.create()
+    return thread
